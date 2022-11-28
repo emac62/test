0e34fdad91e140487eaaab9a57ae773e92ee24ff
emac62 <borderlineboomer@gmail.com>
2022-11-28 13:00:49 -0500

    api

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const url = 'https://script.google.com/macros/s/AKfycbxalPGjmVJzn33FTkHW5l40HXWR4AvF1yXdXUYshf3DQ1mLl_BGaAHsMJROix-TrJi3bw/exec'
 
+const apiKey = 'AIzaSyCuO016qB5uToWhGOeJhFQ_t-RAnjasAtA'
+
+//getData();
+
+function getData() {
+  fetch(url).then((res) => {
+    return res.json()
+  }).then((data) => {
+    console.log(data)
+  })
+}

55b5df0c98baa75718caf2e1f46c7ead334fafb7
emac62 <borderlineboomer@gmail.com>
2022-11-28 14:57:09 -0500

    remove api

--- main.js
+++ main.js
@@ -1,64 +1,33 @@
-const url = 'https://script.google.com/macros/s/AKfycbxalPGjmVJzn33FTkHW5l40HXWR4AvF1yXdXUYshf3DQ1mLl_BGaAHsMJROix-TrJi3bw/exec'
+const url = 'https://docs.google.com/spreadsheets/d/1Xp3OfSFERhzwr8-HB8Yu_QcNAWKqGL3n_aAQkNYV_8k/edit?usp=sharing'
 
-const apiKey = 'AIzaSyCuO016qB5uToWhGOeJhFQ_t-RAnjasAtA'
-
-
+const url2 = 'https://spreadsheets.google.com/feeds/cells/1Xp3OfSFERhzwr8-HB8Yu_QcNAWKqGL3n_aAQkNYV_8k/1/public/full?alt=json'
 
 
+
+
+const url3 = 'https://opensheet.elk.sh/1Xp3OfSFERhzwr8-HB8Yu_QcNAWKqGL3n_aAQkNYV_8k/1'
+var currentQuote = ""
+var currentAuthor = ""
+var currentContext = ""
+var currentTheme = ""
+
+getData();
 
 function getData() {
-  fetch(url, {
-    mode: 'no-cors',
-    header: {
-      'Access-Control-Allow-Origin': 'http://127.0.0.1:5000',
-      'Content-Type:': 'application/json'
-    }
-  }).then((res) => {
-    return res.json()
-  }).then((data) => {
-    console.log(data)
+  fetch(url3, {
+    // mode: 'no-cors',
+    // header: {
+    //   'Access-Control-Allow-Origin': 'http://127.0.0.1:5500/index.html',
+    //   'Content-Type:': 'application/json'
+    // }
+  }).then((res) =>
+
+    res.json()
+  ).then((data) => {
+    console.log(typeof data)
+    let dailyQuote = data[208]
+    currentQuote = dailyQuote["Quote"]
+    console.log(currentQuote)
   })
 }
 
-
-function initClient() {
-  var API_KEY = 'AIzaSyCuO016qB5uToWhGOeJhFQ_t-RAnjasAtA';  // TODO: Update placeholder with desired API key.
-
-  var CLIENT_ID = '101016164355349707085';  // TODO: Update placeholder with desired client ID.
-
-  // TODO: Authorize using one of the following scopes:
-  //   'https://www.googleapis.com/auth/drive'
-  //   'https://www.googleapis.com/auth/drive.file'
-  //   'https://www.googleapis.com/auth/drive.readonly'
-  //   'https://www.googleapis.com/auth/spreadsheets'
-  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-  var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
-
-  gapi.client.init({
-    'apiKey': API_KEY,
-    'clientId': CLIENT_ID,
-    'scope': SCOPE,
-    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  }).then(function () {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  });
-}
-
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-function updateSignInStatus(isSignedIn) {
-  if (isSignedIn) {
-    read_data();
-  }
-}
-
-function handleSignInClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-function handleSignOutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
